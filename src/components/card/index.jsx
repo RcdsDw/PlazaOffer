20c03a79833cc497ac178973c3dc26c756f984ad
Rafael CDS <rafael.candidodasilva.pro@gmail.com>
2023-10-20 09:42:58 +0200

    fix app for deploy build

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -4,7 +4,7 @@ import "./card.css"
 export default function HouseCard({ photos, ville, code_postal, prix_total, surface_m2, description}) {
     return (
         <div className="card">
-                <img className="card__image" src={photos} alt="photo de l'offre" width={450}/>
+                <img className="card__image" src={photos} alt="offre immobiliere" width={450}/>
             <div className="card__content">
             <p className="card__title">{ville}, {code_postal}</p>
             <p className="card__description">{description}</p>
